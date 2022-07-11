bb7a21177b5965e53d00fea6875f02b9852c6ac5
jeremananascto <jeremana1@gmail.com>
2022-07-11 10:28:14 -0300

    first commit

---
+++ src/test/java/Pojo/Usuario.java
@@ -0,0 +1,27 @@
+package Pojo;
+
+public class Usuario {
+
+    private String name;
+    private String job;
+
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getJob() {
+        return job;
+
+    }
+}
+
