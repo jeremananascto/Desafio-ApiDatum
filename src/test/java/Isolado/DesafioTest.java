e82d11f36819f0cfa11a8fb253ddb97c5967d591
Jeremias Nascimento <89275608+jeremananascto@users.noreply.github.com>
2022-07-12 11:48:46 -0300

    Update DesafioTest.java
    incluindo arquivos dentro da pasta de teste, pois foi vazia no Commit

--- src/test/java/Isolado/DesafioTest.java
+++ src/test/java/Isolado/DesafioTest.java
@@ -1,2 +1,51 @@
-package Isolado;public class DesafioTest {
+package Isolado;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import Pojo.Usuario;
+
+
+public class DesafioTest {
+
+    @Test
+    public void Testcadastro() {
+        //configuraçoes de parametros restassured
+        Usuario name = new Usuario();
+        name.setname("jeremias");
+        name.setJob("analista de qa");
+
+        given().log().all()
+                .contentType(ContentType.JSON)
+
+                .body(name)
+
+                .when().post("https://reqres.in/api/users")
+                .then().
+                assertThat().statusCode(201)
+                .body("name", equalTo("jeremias"))
+                .body("job", equalTo("analista de qa"));
+    }
+
+
+    @Test
+    public void TestcadastroValidacaocontrato() {
+        //configuraçoes de parametros restassured
+        Usuario name = new Usuario();
+        name.setname("jeremias");
+        name.setJob("analista de qa");
+
+        given().log().all()
+                .contentType(ContentType.JSON)
+
+                .body(name)
+
+                .when().post("https://reqres.in/api/users")
+                .then().assertThat().statusCode(201).
+
+                body(matchesJsonSchemaInClasspath("Scheema/PostvalidacaodeContrato.json"));
+    }
 }
